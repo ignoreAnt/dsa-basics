3d66a7a3447e9a469b5be98bf78fdb019e16d821
Aakash B <aakash.bamne@gmail.com>
2024-03-19 19:47:00 +0530

    Adds string reverse

---
+++ src/main/java/com/dsa/strings/StringReverse.java
@@ -0,0 +1,37 @@
+package com.dsa.strings;
+
+import java.util.Arrays;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        String str = "hello world!";
+        System.out.println(str);
+        System.out.println(reverse(str));
+    }
+
+    public static String reverse(String s){
+        char[] chars = s.toCharArray();
+
+        int i = 0;
+        int j = chars.length - 1;
+
+        while (i < j){
+            swap(i, j, chars);
+            i++;
+            j--;
+        }
+
+        return new String(chars);
+        // Time complexity : O(n)
+        // Space Complexity : O(n)
+    }
+
+
+    private static void swap(int i, int j, char[] chars){
+         char temp = chars[i];
+         chars[i] = chars[j];
+         chars[j] = temp;
+    }
+
+
+}
---
+++ src/test/java/com/dsa/arrays/Exercise1Test.java
@@ -0,0 +1,40 @@
+package com.dsa.arrays;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Exercise1Test {
+
+    @ParameterizedTest
+    @CsvSource({
+            "1; 1",
+            "1, 1; 1",
+            "1, 2, 3; 2",
+            "; 0"
+    })
+    @DisplayName("Average of elements in array")
+    void shouldReturnValidAverage(int[] input, int expected){
+        // Arrange
+
+        // Act
+        int actual = Exercise1.average(input);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3;true", "4,5,6;false"}) // CSV format with expected output
+    public void checkSum_ShouldReturnEvenSumResult(int[] numbers, boolean isEvenSum) {
+        int sum = Arrays.stream(numbers).sum();
+        assertEquals(isEvenSum, sum % 2 == 0);
+    }
+
+
+}
