5d424961565352aeeebf2c2558512c72689c43bc
Aakash B <aakash.bamne@gmail.com>
2024-03-21 19:50:25 +0530

    Adds counting of digits

--- src/main/java/com/dsa/arrays/Exercise1.java
+++ src/main/java/com/dsa/arrays/Exercise1.java
@@ -1,19 +1,44 @@
 package com.dsa.arrays;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Exercise1 {
     public static void main(String[] args) {
-        System.out.println(average(new int[]{1, 2, 3, 4, 5, 6}));
-        System.out.println(Arrays.toString(reverse(new int[]{1, 2, 3, 4, 5, 6})));
-        int[] ints = new int[]{1, 2, 3, 4, 5}; // {5, 4, 3, 2, 1}
+        int n = 1567; // ==> 7651
+        System.out.println(1567 / 10);  // ==> reduce a number by 1 digit , divide it by 10
+        System.out.println(1567 % 10);  // ==> extract the left most digit of the number, % by 10
 
-        System.out.println(Arrays.toString(ints));
-        System.out.println(Arrays.toString(reverseArrayInplace(ints)));
+        // remainder * 10 +
+        System.out.println(10 * 7 + 1);
+//        int n = 1;
+//        int count = 0;
+//        for (;n != 0; n = n/10) {
+//            count++;
+//        }
+//
+//        while (n != 0){
+//            n = n/10;
+//            count++;
+//        }
+//
+//        System.out.println(count);
 
-        System.out.println();
-        printPairs(new int[]{1, 2, 3});
-        System.out.println(Arrays.toString(twoSum(new int[]{12, 34, 98}, 46)));
+//        System.out.println(average(new int[]{1, 2, 3, 4, 5, 6}));
+//        System.out.println(Arrays.toString(reverse(new int[]{1, 2, 3, 4, 5, 6})));
+//        int[] ints = new int[]{1, 2, 3, 4, 5}; // {5, 4, 3, 2, 1}
+//
+//        System.out.println(Arrays.toString(ints));
+//        System.out.println(Arrays.toString(reverseArrayInplace(ints)));
+//
+//        System.out.println();
+//        printPairs(new int[]{1, 2, 3});
+//        System.out.println(Arrays.toString(twoSum(new int[]{12, 34, 98}, 46)));
+//
+//        System.out.println(Arrays.toString(twoSumV2(new int[]{3, 2, 4}, 6)));
 
 
     }
@@ -100,6 +125,7 @@ public class Exercise1 {
 
     public static int[] twoSum(int[] arr, int target){
         int[] indices = new int[2];
+
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] + arr[j] == target){
@@ -114,12 +140,42 @@ public class Exercise1 {
 
         return new int[]{};
 
-        // Time Complexity :
-        // Space Complexity :
+        // Time Complexity : O(n2)
+        // Auxiliary space : O(1)
     }
 
-    public static int[] printReverse(int[] arr){
+    public static int[] twoSumV2(int[] arr, int target){
 
-        return new int[]{};
+        Map<Integer, Integer> unique = new HashMap<>();
+        int[] result = new int[2];
+        // key --> value
+        for (int i = 0; i < arr.length; i++) { // O(n)
+            unique.put(arr[i], i);
+        }
+
+        for (int i = 0; i < arr.length; i++) {  // O(n)
+            int elementToSearch = target - arr[i];
+            if(unique.containsKey(elementToSearch)){ //O(1)
+                result[0] = i;
+                result[1] = unique.get(elementToSearch);
+                return result;
+            }
+        }
+
+        // time complexity :  O(n) + O(n) ==> 2 O(n) ==> O(n)
+        // Auxiliary space :  O(n)
+        return result;
+    }
+
+    public static int countDigits(int n){
+        int count = 0;
+
+        while (n != 0){
+            n = n/10;
+            count++;
+        }
+
+
+        return count;
     }
 }
--- src/main/java/com/dsa/maths/ReverseNumber.java
+++ src/main/java/com/dsa/maths/ReverseNumber.java
@@ -21,6 +21,7 @@ public class ReverseNumber {
 
     public static int reverseNumber(int n){
         int reverse = 0;
+
         while (n != 0){
             reverse = (reverse * 10) + (n % 10);
             n = n / 10;
